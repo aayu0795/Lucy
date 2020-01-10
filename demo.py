a63597ab4910ecb5cec36af67bfb021bef828b74
aayu0795 <aayushkumar0795@gmail.com>
2020-01-11 00:23:47 +0530

    Almost done!

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
-import os
-
-os.startfile('computer.lnk')
+import google
+num_page = 3
+search_results = google.search("chapak release date", num_page)
+for result in search_results:
+    print(result.description)
